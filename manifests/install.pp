c23aba546c1d4aec7cb9fe2153e32530321a4705
shazi7804 <shazi7804@gmail.com>
2017-06-27 14:49:32 +0800

    fix rake

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,11 @@
 class java::install inherits java {
-  if $jdk_type == 'oraclejdk' {
+  if $java::jdk_type == 'oraclejdk' {
     include java::install::oracle_jdk
   }
-  elsif $jdk_type == 'openjdk' {
+  elsif $java::jdk_type == 'openjdk' {
     include java::install::open_jdk
   }
   else {
-    fail ("unsupported jdk type ${jdk_type}")
+    fail ("unsupported jdk type ${java::jdk_type}")
   }
 }
